76cb250fe2904cb950e33806cd1083d530894001
petrchovanec <chovanec@gmail.com>
2017-05-30 23:02:48 -0400

    solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+## This function creates an object (a list) with matrix and its cashed inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverseMatrix <- NULL
+  setMatrix <- function(mat){
+    x <<- mat
+    inverseMatrix <<- NULL
+  }
+  getMatrix <- function() x
+  setInvMatrix <- function(invMat) inverseMatrix <<- invMat
+  getInvMatrix <- function() inverseMatrix
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInvMatrix = setInvMatrix, getInvMatrix = getInvMatrix)
 }
 
-
-## Write a short comment describing this function
+## Cashing an inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invMat <- x$getInvMatrix()
+  if (!is.null(invMat)){
+    message("Getting cached inverse matrix")
+    return(invMat)
+  }
+  mat <- x$getMatrix()
+  invMat <- solve(mat)
+  x$setInvMatrix(invMat)
+  invMat
 }
